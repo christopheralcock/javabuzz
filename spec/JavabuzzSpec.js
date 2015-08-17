9e629b989a0f81bcc6388b01d2749d91bfabfb23
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-17 11:24:38 +0100

    all fizzbuzz functions working

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -2,17 +2,39 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+   javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by three', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
+
+    it('divisible by five', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by three and five', function() {
+      expect(javabuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    });
+
   });
   describe('knows when a number is NOT', function(){
     it('divisible by three', function (){
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
+
+    it('divisible by five', function (){
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
+    it('divisible by three and five', function() {
+      expect(javabuzz.isDivisibleByThreeAndFive(1)).toBe(false);
+    });
+
   });
+
+
 });
