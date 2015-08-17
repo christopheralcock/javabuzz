447376a18c6de1c1ab541ece7428383a23d04e43
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-17 11:57:39 +0100

    completed says method

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,44 +1,61 @@
 describe('Javabuzz', function() {
 
-  var javabuzz;
+ var javabuzz;
 
-  beforeEach(function() {
-   javabuzz = new Javabuzz();
-  });
+ beforeEach(function() {
+  javabuzz = new Javabuzz();
+ });
 
-  describe('knows when a number is', function() {
+ describe('knows when a number is', function() {
 
-    it('divisible by three', function() {
-      expect(javabuzz._isDivisibleBy(3,3)).toBe(true);
-    });
+   it('divisible by three', function() {
+     expect(javabuzz._isDivisibleBy(3,3)).toBe(true);
+   });
 
-    it('divisible by three', function() {
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
-    });
+   it('divisible by three', function() {
+     expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+   });
 
-    it('divisible by five', function() {
-      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
-    });
+   it('divisible by five', function() {
+     expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+   });
 
-    it('divisible by three and five', function() {
-      expect(javabuzz.isDivisibleByThreeAndFive(15)).toBe(true);
-    });
+   it('divisible by three and five', function() {
+     expect(javabuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+   });
 
-  });
-  describe('knows when a number is NOT', function(){
-    it('divisible by three', function (){
-      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
-    });
+ });
 
-    it('divisible by five', function (){
-      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
-    });
+ describe('knows when a number is NOT', function(){
+   it('divisible by three', function (){
+     expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+   });
 
-    it('divisible by three and five', function() {
-      expect(javabuzz.isDivisibleByThreeAndFive(1)).toBe(false);
-    });
+   it('divisible by five', function (){
+     expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+   });
 
-  });
+   it('divisible by three and five', function() {
+     expect(javabuzz.isDivisibleByThreeAndFive(1)).toBe(false);
+   });
+
+ });
+
+ describe('when playing, says', function() {
+
+   it('"Java" when a number is divisible by 3', function() {
+     expect(javabuzz.says(3)).toEqual("Java");
+   });
+
+   it('"Buzz" when a number is divisible by 5', function() {
+     expect(javabuzz.says(5)).toEqual("Buzz");
+   });
+
+   it('"JavaBuzz" when a number is divisible by 3 and 5', function() {
+     expect(javabuzz.says(15)).toEqual("JavaBuzz");
+   });
 
 
-});
+ });
+
+ });
